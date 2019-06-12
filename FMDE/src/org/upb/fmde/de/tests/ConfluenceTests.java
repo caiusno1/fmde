eb423f46ef95555f468d4786499bf1a59542a624
daniel <daniel@daniel-Aspire-5755G>
2019-06-12 14:26:30 +0200

    Added new Testclass for the confluence tests

---
+++ FMDE/src/org/upb/fmde/de/tests/ConfluenceTests.java
@@ -0,0 +1,25 @@
+package org.upb.fmde.de.tests;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.upb.fmde.de.categories.concrete.finsets.OpFinSets.OpFinSets;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.upb.fmde.de.categories.concrete.finsets.CounterExampleChecker;
+import org.upb.fmde.de.categories.concrete.finsets.FinSet;
+import org.upb.fmde.de.categories.concrete.finsets.FinSetDiagram;
+import org.upb.fmde.de.categories.concrete.finsets.OpCounterExampleChecker;
+import org.upb.fmde.de.categories.concrete.finsets.TotalFunction;
+import org.upb.fmde.de.categories.diagrams.Diagram;
+
+
+public class ConfluenceTests {
+	@Test
+	public void parallelIndependence() {
+		//TODO: Implement parallelIndependence
+	}
+}
